514547a5200c2ab57c8d14cc046f679744637a00
Abhishek-Patadiya <patadiya2244@gmail.com>
2022-11-13 10:51:08 -0500

    update

--- Customer_Relationship_Management_Springboot_JPA/src/main/java/com/Customer_Relationship_Management/Dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Customer_Relationship_Management.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Customer_Relationship_Management.Entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
